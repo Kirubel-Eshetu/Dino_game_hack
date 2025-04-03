3e4b6f3c0cf1e58826506ee8d777f6b512c5a5d7
Kirubel Eshetu Tefera <Kirubelwinner@gmail.com>
2025-04-03 08:42:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Code to getinto the dino game in goggle chrome
 
+// The maximum score the code gives you is 999999 before it resets the score to 0.
 // Setup the jump Limit
-Runner.instance_.tRex.setJumpVelocity(15);  // This code sets up the jump limit of the dino
+Runner.instance_.tRex.setJumpVelocity(15);  // This code sets up the jump limit of the dino in pixels
 
 // Modify how fast the dino runs
-Runner.instance_.setSpeed(2500);        //setup the speed of the dinosaur to any number
+Runner.instance_.setSpeed(2500);        //setup the speed of the dinosaur in pixels per second (px/s)
 Runner.instance_.setSpeed(-1);         //Make the dinosur to go backwards when using negative values
 
 // Hack the game ( Make the dinosaur run without being doddged by the obstacles)
