a434f23fafb9559abe3cf6a653dd9dfabad7e9ce
Kirubel Eshetu Tefera <Kirubelwinner@gmail.com>
2025-04-03 08:00:48 +0300

    Update script.js
    Added extended comments to the source code for user to understand.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Code to make the Dinosaur to simply go through all the obstacles without doddging
 
-var dummy = Runner.prototype.gameOver   // This is the original gameOver function assigned to a variable called dummy
-Runner.protoype.gameOver = function(){
+var dummy = Runner.prototype.gameOver     // This is the original gameOver function assigned to a variable called dummy
+Runner.protoype.gameOver = function(){    // gameOver function was overridden (replaced) by an empty function having no functionality
   
-}
+}  // Since the gameOver function was overridden by an empty one, the game is customized and the dino will not dodge to any obstacles
