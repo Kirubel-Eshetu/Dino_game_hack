0b6785fc66ede2cd49333059cf1fd6c63808413a
Kirubel Eshetu Tefera <Kirubelwinner@gmail.com>
2025-04-03 08:38:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 Runner.instance_.tRex.setJumpVelocity(15);  // This code sets up the jump limit of the dino
 
 // Modify how fast the dino runs
-Runner.instance_.setSpeed(2500);         /* setup the speed of the dinosaur to any number. ) */
-Runner.instance_.setSpeed(-1);         //Make the dinosur to go backwards
+Runner.instance_.setSpeed(2500);        //setup the speed of the dinosaur to any number
+Runner.instance_.setSpeed(-1);         //Make the dinosur to go backwards when using negative values
 
 // Hack the game ( Make the dinosaur run without being doddged by the obstacles)
 var dino = Runner.prototype.gameOver;     // This is the original gameOver function assigned to a variable called dino
@@ -13,6 +13,7 @@ Runner.protoype.gameOver = function(){    // gameOver function was overridden (r
 }  // Since the gameOver function was overridden by an empty one, the game is customized and the dino will not dodge to any obstacles
 
 // Stop the game
-Runner.prototype.gameOver = dino;
+Runner.prototype.gameOver = dino; /* Since the game no longer has a collision stop mechanism after running 
+"Hack the game" code, it will run forever. If you want to stop it, you need to employ the Console again. */
 
 
