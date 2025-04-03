76a448dffc8926de932c148dac6fa7a44080621a
Kirubel Eshetu Tefera <Kirubelwinner@gmail.com>
2025-04-03 08:53:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,10 @@ Runner.instance_.setSpeed(-1);         //Make the dinosur to go backwards when u
 
 // Hack the game ( Make the dinosaur run without being doddged by the obstacles)
 var dino = Runner.prototype.gameOver;     // This is the original gameOver function assigned to a variable called dino
-Runner.protoype.gameOver = function(){    // gameOver function was overridden (replaced) by an empty function having no functionality
-}  // Since the gameOver function was overridden by an empty one, the game is customized and the dino will not dodge to any obstacles
+Runner.protoype.gameOver = function(){    /* gameOver function was overridden (replaced) by an empty function 
+  having no functionality */
+}  /* Since the gameOver function was overridden by an empty one, the game is customized and the dino 
+will not dodge to any obstacles */
 
 // Stop the game
 Runner.prototype.gameOver = dino; /* Since the game no longer has a collision stop mechanism after running 
