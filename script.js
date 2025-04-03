ef0b8ca9ed0e9dc60b178f328df892a5988abc48
Kirubel Eshetu Tefera <Kirubelwinner@gmail.com>
2025-04-03 07:56:51 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Code to make the Dinosaur to simply go through all the obstacles without doddging
+
+var dummy = Runner.prototype.gameOver   // This is the original gameOver function assigned to a variable called dummy
+Runner.protoype.gameOver = function(){
+  
+}
